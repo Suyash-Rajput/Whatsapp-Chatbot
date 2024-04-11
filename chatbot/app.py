57e5bbe9541fc664f78391f36ba47235f59b9fe7
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2024-04-11 14:05:30 +0530

    changes

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 import gemini 
-import api_whatsapp as api_wa
+from api_whatsapp import API_Whatsapp
+
+
 app = Flask(__name__)
 
-@app.route('/whatsapp', methods = ['POST'])
+@app.route('/whatsapp', methods=['POST'])
 def wa_reply():
     query = request.form.get('Body').lower()
-    print("User query  %s" % query)
-    generate_ans =  gemini.get_gemini_response(query)
-    return   api_wa.message_2(generate_ans)
+    print("User query: %s" % query)
+    generate_ans = gemini.get_gemini_response(query)
+    wa_api = API_Whatsapp()
+    response = wa_api.message_2(generate_ans)
+    return str(response.body)
+
+if __name__ == "__main__":
+    app.run(debug=True)
