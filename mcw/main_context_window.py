504751ba36faa0770bfb369428c90b437cc7349f
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2024-04-17 18:39:00 +0530

    changes regarding maintaining session

--- mcw/main_context_window.py
+++ mcw/main_context_window.py
@@ -1,58 +1,85 @@
 import utils 
+from datetime import datetime, timezone
+from chatbot import prompts as pt
 
 BASE_DIR = Path(__file__).resolve().parent
 parent_dir = os.path.dirname(BASE_DIR)
 
 state_transition_obj = {}
-mcw_hint_2_state = {}
+mcw_name_2_state = {}
 
 def load_states(flow):
     state_transition_pfn = os.path.join(parent_dir, 'json_files/state_transition.json')
     global state_transition_obj
-    global mcw_hint_2_state
+    global mcw_name_2_state
     state_transition_obj = utils.read_json_file(state_transition_pfn)
     state_transition_states = state_transition_obj["states"]
     for next_state in state_transition_states:
-        mcw_hint = state_transition_states[next_state]["mcw_hint"]
-        mcw_hint_2_state[mcw_hint] = next_state
+        name = state_transition_states[next_state]["name"]
+        mcw_name_2_state[name] = next_state
     return
 
 
 def fill_initial_state(session):
+    print("Entering fill_initial_state: ", session)
     state_transition_path =  os.path.join(parent_dir, 'json_files/state_transition.json')
     state_transition = utils.read_json_file(state_transition_path)
     questions_list = state_transition["states"]['S0']
-    session['id'] = questions_list['id']
-    session['check_file'] = questions_list['check_file']
-    
-    for key, values in questions_list['conditions'].items():
-        session[key] = values
-
-    for key, values in questions_list['outgoing'].items():
-        session[key] = values
-
+    session['__check_file'] = questions_list['check_file']
+    session["__user_pref"] = questions_list['outgoing'][0]['user_pref']
+    session["__state"] = questions_list['id']
+    session["__target_state"] =  questions_list['outgoing'][0]['id']
+    session["__prompt"] = questions_list['outgoing'][0]["prompt"]
+    session['__question'] = state_transition["config"][session['__target_state']]['question']
+    print("Exiting fill_initial_state(): ", session)
+    return
 
 def find_state(session):
-    print("Entering find_state: ", context_window)
-    # if len(state_transition_obj) == 0:
+    print("Entering find_state: ", session)
     load_states(flow)
     state_transition_config = state_transition_obj["config"]
-    mcw_hint = ""
+    mcw_name = ""
     for next_filter in state_transition_config:
-        if next_filter == "feedback":
-            continue
-        if len(context_window[next_filter]) != 0:
-            mcw_hint += next_filter
-    if mcw_hint == "":
-        mcw_hint = "start"
-    if len(mcw_hint_2_state) == 0:
-        load_states(flow)
-    my_state = mcw_hint_2_state[mcw_hint]
-    print("Exiting check_state2(): ", my_state)
+        if len(session[next_filter]) != 0:
+            mcw_name += next_filter
+    my_state = mcw_hint_2_state[mcw_name]
+    print("Exiting find_state(): ", my_state)
     return my_state
         
+        
+def set_target_state(session):
+    print("Entering set_target_state() search_query: ", session)
+    global state_transition_obj
+    current_state = session["__state"]
+    target_state = state_transition_obj["states"][current_state]["outgoing"][0]
+    ts, up, q = "", "", ""
+    if "user_pref" in target_state.keys():
+        up = state_transition_obj["states"][current_state]["outgoing"][0]["user_pref"]
+        if up != "":
+            ts = target_state["id"]
+            q = state_transition_obj["config"][up]["question"]
+    session["__target_state"] = ts
+    session["__user_pref"] = up
+    session['__check_file'] = state_transition_obj["states"][ts]['check_file']
+    session["__question"] = q
+    session["__prompt"] = state_transition_obj["states"]["outgoing"][0]["prompt"]
+    print("Exiting set_target_state() search_query: ", session)
+    return
 
-def update_context(session, context):
-    if len(session) == 0:
-       fill_initial_state(session)
-    currect_state = find_state(session)
+
+def update_context(session, query):
+    session_started_time = session.get('time_started', None)
+    time_diff = 12
+    if session_started_time is not None:
+        time_diff = utils.print_time_taken(session['time_started'], datetime.now())
+        
+    if 'To' in session and session['To'] == recipient_number and time_diff < 10:
+        if len(session) == 0:
+           fill_initial_state(session)
+        session["__state"] = find_state(session)
+        set_target_state(session)
+    else: 
+        session['time_started'] = datetime.now()  
+        fill_initial_state(session) 
+    session['To'] = recipient_number
+    return
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import os, csv
+
 
 
 def read_json_file(file_path):
@@ -7,3 +9,21 @@ def read_json_file(file_path):
     with open(file_path, "r", encoding="utf-8") as json_file:
         my_json_data = json.load(json_file)
     return my_json_data
+
+def print_time_taken(start_time, end_time):
+    end_time_aware = end_time.replace(tzinfo=timezone.utc)
+    duration = end_time_aware - start_time
+    total_time_seconds = duration.total_seconds()
+    print(f"Time taken - : {total_time_seconds:.6f} seconds")
+    seconds = round(total_time_seconds, 0)
+    minutes = seconds // 60
+    return minutes
+
+
+def read_csv(file_path):
+    data = []
+    with open(file_path, 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return data
