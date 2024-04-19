7d9aa5ed31193d54ad89497bcaf10402373367a9
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2024-04-19 09:12:24 +0530

    changes related to session

--- mcw/main_context_window.py
+++ mcw/main_context_window.py
@@ -1,14 +1,15 @@
-import utils 
+import utils ,os 
 from datetime import datetime, timezone
-from chatbot import prompts as pt
-
+from chatbot import gemini
+from chatbot import  prompts as pt
+from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 parent_dir = os.path.dirname(BASE_DIR)
 
 state_transition_obj = {}
 mcw_name_2_state = {}
 
-def load_states(flow):
+def load_states():
     state_transition_pfn = os.path.join(parent_dir, 'json_files/state_transition.json')
     global state_transition_obj
     global mcw_name_2_state
@@ -26,24 +27,23 @@ def fill_initial_state(session):
     state_transition = utils.read_json_file(state_transition_path)
     questions_list = state_transition["states"]['S0']
     session['__check_file'] = questions_list['check_file']
+    session['__name'] =  questions_list['name']
     session["__user_pref"] = questions_list['outgoing'][0]['user_pref']
     session["__state"] = questions_list['id']
     session["__target_state"] =  questions_list['outgoing'][0]['id']
     session["__prompt"] = questions_list['outgoing'][0]["prompt"]
-    session['__question'] = state_transition["config"][session['__target_state']]['question']
+    session['__question'] = state_transition["config"][session['__user_pref']]['question']
     print("Exiting fill_initial_state(): ", session)
     return
 
 def find_state(session):
-    print("Entering find_state: ", session)
-    load_states(flow)
+    load_states()
     state_transition_config = state_transition_obj["config"]
     mcw_name = ""
     for next_filter in state_transition_config:
-        if len(session[next_filter]) != 0:
+        if session['__user_pref'] == next_filter:
             mcw_name += next_filter
-    my_state = mcw_hint_2_state[mcw_name]
-    print("Exiting find_state(): ", my_state)
+    my_state = mcw_name_2_state[session['__name']]
     return my_state
         
         
@@ -51,27 +51,28 @@ def set_target_state(session):
     print("Entering set_target_state() search_query: ", session)
     global state_transition_obj
     current_state = session["__state"]
-    target_state = state_transition_obj["states"][current_state]["outgoing"][0]
-    ts, up, q = "", "", ""
-    if "user_pref" in target_state.keys():
-        up = state_transition_obj["states"][current_state]["outgoing"][0]["user_pref"]
-        if up != "":
-            ts = target_state["id"]
-            q = state_transition_obj["config"][up]["question"]
-    session["__target_state"] = ts
-    session["__user_pref"] = up
-    session['__check_file'] = state_transition_obj["states"][ts]['check_file']
-    session["__question"] = q
-    session["__prompt"] = state_transition_obj["states"]["outgoing"][0]["prompt"]
+    target_state = state_transition_obj["states"][current_state]["outgoing"][0]["id"]
+    user_pref =  state_transition_obj["states"][target_state]["outgoing"][0]["user_pref"]
+    session["__target_state"] = target_state
+    session['__name'] =  state_transition_obj["states"][target_state]['name']
+    session["__user_pref"] = user_pref
+    session['__check_file'] = state_transition_obj["states"][target_state]['check_file']
+    session["__question"] =   state_transition_obj["config"][user_pref]["question"]
+    session["__prompt"] = state_transition_obj["states"][target_state]["outgoing"][0]["prompt"]
     print("Exiting set_target_state() search_query: ", session)
     return
 
 
-def update_context(session, query):
+def update_context(session, query, recipient_number):
     session_started_time = session.get('time_started', None)
-    check_status =  pt.check_template(query, session["__check_file"])
-    if check_status.strip().lower() == "no":
-        return 
+    check_file = session.get('time_started', None)
+    if check_file is not None :
+       print('query ;-', query, 'QUESTION ;- ', session['__question'])
+       check_status =  pt.check_template(query, session["__check_file"], session['__question'])
+       generate_ans = gemini.get_gemini_response(check_status)
+       print(" Check status: %s" % generate_ans)
+       if generate_ans.strip().lower() == "no":
+          return 
     time_diff = 12
     if session_started_time is not None:
         time_diff = utils.print_time_taken(session['time_started'], datetime.now())        
@@ -83,5 +84,4 @@ def update_context(session, query):
     else: 
         session['time_started'] = datetime.now()  
         fill_initial_state(session) 
-    session['To'] = recipient_number
     return
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import os, csv
-
-
+import json
+from datetime import datetime, timezone
 
 def read_json_file(file_path):
     my_json_data = {}
-    if not check_file(file_path):
+    if not os.path.exists(file_path):
         return my_json_data
     with open(file_path, "r", encoding="utf-8") as json_file:
         my_json_data = json.load(json_file)
